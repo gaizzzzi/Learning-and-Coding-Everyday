84961f7dde497a24045a24d2bc0fb40cb3d9199f
gaizzzzi <jzhou106@fordham.edu>
2020-05-25 13:40:59 -0400

    fasten 200 by changing the way to initilize visited, deleting width/length, decomposing new_pos to new_x&new_y and using tuple in directions - eliminating numerous lists creation.

--- main/200_Number_of_Islands.py
+++ main/200_Number_of_Islands.py
@@ -3,29 +3,23 @@ class Solution:
         if not grid:
             return 0
         
-        width = len(grid)
-        length = len(grid[0])
+        visited = [[False]* len(grid[0]) for i in range(len(grid))]
         
-        visited = []
-        for i in range(width):
-            tmp = [False for j in range(length)]
-            visited.append(tmp)
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
         
-        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-        
-        def dfs_helper(pos):
-            for direc in directions:
-                new_pos = [pos[0] + direc[0], pos[1] + direc[1]]
-                if 0 <= new_pos[0] < width and 0 <= new_pos[1] < length and not visited[new_pos[0]][new_pos[1]]:
-                    if grid[new_pos[0]][new_pos[1]] == '1':
-                        visited[new_pos[0]][new_pos[1]] = True
-                        dfs_helper(new_pos)
+        def dfs_helper(x, y):
+            for (dx, dy) in directions:
+                new_x, new_y =  x + dx, y + dy
+                if 0 <= new_x < len(grid) and 0 <= new_y < len(grid[0]) and not visited[new_x][new_y]:
+                    if grid[new_x][new_y] == '1':
+                        visited[new_x][new_y] = True
+                        dfs_helper(new_x, new_y)
                         
         island_num = 0
-        for i in range(width):
-            for j in range(length):
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
                 if grid[i][j] == '1' and not visited[i][j]:
                     island_num += 1
-                    dfs_helper([i, j])
+                    dfs_helper(i, j)
         
         return island_num
