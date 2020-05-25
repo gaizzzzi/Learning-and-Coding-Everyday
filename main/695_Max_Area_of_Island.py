3bb0fc71db330cbcd08a16b93601eea215700968
gaizzzzi <jzhou106@fordham.edu>
2020-05-25 15:08:09 -0400

    improve script by returning an area

--- main/695_Max_Area_of_Island.py
+++ main/695_Max_Area_of_Island.py
@@ -6,21 +6,22 @@ class Solution:
         visited = [[False] * len(grid[0]) for i in range(len(grid))]
         directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
         
-        def dfs_helper(x, y, areas):
+        def dfs_helper(x, y):
             if 0 <= x < len(grid) and 0 <= y < len(grid[0]) and not visited[x][y] and grid[x][y] == 1:
                 visited[x][y] = True
-                areas[0] += 1
-                areas[1] = max(areas[0], areas[1])
-                
+                area = 1
                 for (dx, dy) in directions:
                     new_x, new_y = x + dx, y + dy
-                    dfs_helper(new_x, new_y, areas)
-        areas = [0, 0]         
+                    area += dfs_helper(new_x, new_y)
+                return area
+            else:
+                return 0
+
+        max_area = 0        
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == 1 and not visited[i][j]:
-                    areas[0] = 0
-                    dfs_helper(i, j, areas)
-        
-        return areas[1]
-    
+                    
+                    max_area = max(max_area, dfs_helper(i, j))
+                    
+        return max_area
