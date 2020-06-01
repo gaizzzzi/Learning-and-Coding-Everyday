0ad0e4ede8dba97319d1b5fffd5ef97d8ead81eb
gaizzzzi <65695699+gaizzzzi@users.noreply.github.com>
2020-05-31 23:04:00 -0400

    added 307 (#18)

--- main/3_Longest_Substring_Without_Repeating_Characters.py
+++ main/3_Longest_Substring_Without_Repeating_Characters.py
@@ -8,7 +8,7 @@ class Solution:
         letter_map = {}
         while end < len(s):
             if letter_map.get(s[end]) is not None:
-            start = max(letter_map[s[end]] + 1, start)
+                start = max(letter_map[s[end]] + 1, start)
             letter_map[s[end]] = end
             maxlength = max(maxlength, end - start + 1)
             end += 1
