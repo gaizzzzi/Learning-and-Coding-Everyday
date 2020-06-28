c84190bb1aa6e89b6e09c876a5ae1d9af90e6f26
gaizzzzi <65695699+gaizzzzi@users.noreply.github.com>
2020-06-28 11:48:32 -0400

    added binary search method for 1300 (#43)

--- main/1300_Sum_of_Mutated_Array_Closest_to_Target.py
+++ main/1300_Sum_of_Mutated_Array_Closest_to_Target.py
@@ -6,3 +6,29 @@ class Solution:
             n -= 1
             target -= arr[n]
         return round((target - 0.0001) / n) if n else arr[0]
+
+    def findBestValue(self, arr: List[int], target: int) -> int:
+        def get_sum(mid):
+            tmp_sum = 0
+            for num in arr:
+                tmp_sum += min(num - mid, 0) + mid
+            return tmp_sum
+        
+        l, r = 0, max(arr)
+        
+        while l < r - 1:
+            mid = (l + r) // 2
+            tmp_sum = get_sum(mid)
+            if tmp_sum == target:
+                return mid
+            elif tmp_sum < target:
+                l = mid
+            else:
+                r = mid
+                
+        l_v = abs(get_sum(l) - target) 
+        r_v = abs(get_sum(r) - target)
+        if l_v > r_v:
+            return r
+        else:
+            return l
